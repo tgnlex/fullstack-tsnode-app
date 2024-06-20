5bdf36dae85b0c77f9b5693008cdc6ba36f4e111
tgnlex <alexanderwebber76@gmail.com>
2024-06-20 07:28:26 -0400

    fixed port conflict

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import {server} from './server.ts';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const host = process.env.HOST || "localhost";
 
 function main(){
