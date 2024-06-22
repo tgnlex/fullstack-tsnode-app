bebae32a66f7614d383c8297ed253f6c6738168a
Tgn <114723993+tgnlex@users.noreply.github.com>
2024-06-22 19:18:21 -0400

    Update sql.ts

--- backend/src/db/sql.ts
+++ backend/src/db/sql.ts
@@ -1,5 +1,10 @@
 import db, {closeDB} from './db.ts'
-
+interface DBMigration {
+  version: number,
+  up: string | ((db: Database) => void),
+  down: string | ((db: Database) => void)
+}
+function migrate(db: Database, migrations: DBMigration[], targetVersion?: number): void 
 const sql = (query) => {
   db.serialize(() => {
     const stmt = db.prepare(`${query}`)
