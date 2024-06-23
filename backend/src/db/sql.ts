16bc021494db4b5262710209ddaf099c1e576e5d
tgnlex <alexanderwebber76@gmail.com>
2024-06-23 09:55:13 -0400

    update sql.ts

--- backend/src/db/sql.ts
+++ backend/src/db/sql.ts
@@ -1,5 +1,10 @@
 import db, {closeDB} from './db.ts'
-
+interface DBMigration {
+  version: number,
+  up: string | ((db: Database) => void),
+  down: string | ((db: Database) => void)
+}
+function migrate(db: Database, migrations: DBMigration[], targetVersion?: number): void 
 const sql = (query) => {
   db.serialize(() => {
     const stmt = db.prepare(`${query}`)
@@ -10,7 +15,7 @@ const sql = (query) => {
   };
   closeDB()
 };
-class Controller = {
-  constructor(db: Database)
-};
+class Controller {
+  constructor(private db: Database) {}
+}
 export {sql, Controller}
