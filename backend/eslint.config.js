523a29747bebcd02f563b99c517604d81cc39779
tgnlex <alexanderwebber76@gmail.com>
2024-06-30 00:12:49 -0400

    update

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -3,15 +3,25 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  env: {es2021: true, node: true},
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+  extends: {
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings,'
+		'plugin:import/typescript,'
+  }
   {
     languageOptions: {
-      parserOptions: {
+        parser: '@typescript-eslint/parser',
+				parserOptions: {
         project: true,
 				tsconfigRootDir: import.meta.dirname,
       },
+
     },
   },
   {
